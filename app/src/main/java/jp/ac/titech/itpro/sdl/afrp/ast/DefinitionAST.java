7102f86a3c85cccda02a79c030f222593d331a19
chakku <chakkurai0724@outlook.jp>
2018-07-08 23:44:11 +0900

    DefinitionASTにexecと依存取得関数を追加

--- app/src/main/java/jp/ac/titech/itpro/sdl/afrp/ast/DefinitionAST.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/afrp/ast/DefinitionAST.java
@@ -4,9 +4,14 @@ import jp.ac.titech.itpro.sdl.afrp.AFRPParser;
 
 import android.util.Log;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
 public class DefinitionAST implements AST {
     public String node_id;
     public ExpressionAST expast;
+    public Collection<String> dependence;
 
     public static DefinitionAST parse(AFRPParser.DefinitionContext ctx){
         DefinitionAST ast = new DefinitionAST();
@@ -29,5 +34,21 @@ public class DefinitionAST implements AST {
         str += "\n";
         Log.d("chakku:AST",tabs + str);
         expast.print(tab+2);
+    }
+
+    @Override
+    public void exec(Map<String, Number> map) {
+
+    }
+
+    /* 依存している変数のリストを作成する */
+    public Collection<String> getDependence(){
+        dependence = expast.getDependance();
+        return dependence;
+    }
+
+    /* node <node_id> = <exp> の node_id を返す */
+    public String getNodeName(){
+        return node_id;
     }
 }

5229da5a2619b4dba18de0740b01a375346c527f
chakku <chakkurai0724@outlook.jp>
2018-07-08 23:41:44 +0900

    ASTにexecメソッドを追加

--- app/src/main/java/jp/ac/titech/itpro/sdl/afrp/ast/AST.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/afrp/ast/AST.java
@@ -1,6 +1,8 @@
 package jp.ac.titech.itpro.sdl.afrp.ast;
 
+import java.util.Map;
 public interface AST {
     public void print();
     public void print(int tab);
+    public void exec(Map<String,Number> map);
 }
